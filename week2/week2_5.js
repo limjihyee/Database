cf4f8e6a91101cdb1e1625a6ece4346b75e0bc98
limjihyee <83489228+limjihyee@users.noreply.github.com>
2023-09-10 17:45:18 +0900

    week2 hw

--- week2/week2_5.js
+++ week2/week2_5.js
@@ -1,26 +1,28 @@
-function rand(min, max){
-    return Math.floor(Math.random() * (max -min +1)) + min;
+//random 숫자 도출
+function generateRandomNumber(min, max) { // 함수 이름 수정
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let randomNumber; // 전역 변수 선언
-function getValueInRange(){
-    let inputData = document.getElementById("max-num").value;
-    randomNumber = rand("GEE4011", inputData);
-    console.log(randomNumber);
-    alert("Random Number is Generated!");
-    return randomNumber;
+let randomnum; // 전역 변수로 무작위 숫자 저장하기 위함
+//input 요소에서 사용자가 선택한 숫자 범위 중 무작위 숫자 생성 후 저장
+
+function getchoose(){
+    let inputData = document.getElementById("js-range").value;
+    randomnum = generateRandomNumber(0, inputData); //선택한 숫자 범위 안에서 무작위 숫자 생성
+    return randomnum; // 생성한 숫자 해당 변수에 저장
 }
 
-function match(){
-    document.getElementById('js-result').innerHTML = "";
-    var inputNumber = document.getElementById("input_number").value; 
+// 사용자가 선택한 숫자와 기계가 선택한 숫자 비교
+function compare() {
+    document.getElementById("js-result").innerHTML = ""; // 결과 표시 내용 초기화
+    var inputnum = document.getElementById("num").value;
 
-    document.getElementById('js-result').innerHTML=`You choose: ${inputNumber}, the machine choose: ${randomNumber}`;
+    document.getElementById("js-result").innerHTML = `You choose: ${inputnum}, the machine choose: ${randomnum}.`;
 
-    if(randomNumber == inputNumber){
-        document.getElementById('js-result').insertAdjacentHTML('afterEnd', "<b>You Win!</b>")
-    } else {
-        document.getElementById('js-result').insertAdjacentHTML('afterEnd', "<b>You Lost!</b>")
+    if (randomnum == inputnum) {
+        document.getElementById("js-result").innerHTML += '<br>you win!';
     }
-
+    else {
+        document.getElementById("js-result").innerHTML += '<br>you Lost!';
+    }
 }
